c1219181327df69616e755a10c18befcf715d157
Doga Fincan <doga@icloud.com>
2022-08-24 19:08:55 +0200

    Change test name

--- greetings_test.go
+++ greetings_test.go
@@ -9,7 +9,7 @@ func init() {
 	rand.Seed(1)
 }
 
-func TestHello(t *testing.T) {
+func TestRandomGreeting(t *testing.T) {
 	// Change expected when the greetings array changes.
 	expected := "こんにちは"
 	actual := RandomGreeting()
